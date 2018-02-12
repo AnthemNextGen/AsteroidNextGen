34d322e85ad40e0b4c95babc7edd9a6f2556a755
delveri <delveri.chick@anthem.com>
2018-02-12 12:10:59 -0500

    Added Splashscreen loop

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
-import {Game} from './utils/game';
+import {Game,splashScreen, gameScreen} from './utils/game';
+document.addEventListener('keyup', function(event){
+    if(event.keyCode == 32){
+      splashScreen.className += 'fadesplash';
+      setTimeout(function(){
+        splashScreen.style.display = 'none';
+      }, 3000);
+    }
+});
+
 const game = new Game();
 game.play();
--- src/utils/game.js
+++ src/utils/game.js
@@ -2,16 +2,6 @@ import ioClient from 'socket.io-client';
 export const splashScreen = document.getElementById('splash');
 export const gameScreen = document.getElementById('game');
 
-document.addEventListener('keyup', function(event){
-    if(event.keyCode == 32){
-      splashScreen.className += 'fadesplash';
-      setTimeout(function(){
-        splashScreen.style.display = 'none';
-      }, 3000);
-
-    }
-});
-
 export class Game{
   constructor(){
   let io = ioClient('localhost:5000');
