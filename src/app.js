f5e3d6326de8966dff66d63ea96534601293cde3
delveri <delveri.chick@anthem.com>
2018-02-12 11:14:34 -0500

    Added transitions to Game

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-
-import {sayHello, User} from './utils/functions';
 import {Game} from './utils/game';
-const greeting = 'Welcome NextGen';
-console.log(greeting);
-sayHello();
-
-const user = new User();
-
 const game = new Game();
 game.play();
--- src/utils/game.js
+++ src/utils/game.js
@@ -1,11 +1,24 @@
-import ioClient from 'socket.io-client'
+import ioClient from 'socket.io-client';
+export const splashScreen = document.getElementById('splash');
+export const gameScreen = document.getElementById('game');
+
+document.addEventListener('keyup', function(event){
+    if(event.keyCode == 32){
+      splashScreen.className += 'fadesplash';
+      setTimeout(function(){
+        splashScreen.style.display = 'none';
+      }, 3000);
+
+    }
+});
+
 export class Game{
   constructor(){
   let io = ioClient('localhost:5000');
   io.on('message', function(msg){
     //alert(msg);
   });
-  }
+}
 
   play(){
     console.log('Game playing...');
