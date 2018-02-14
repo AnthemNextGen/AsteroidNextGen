c8cc18e5eebaa96d357ed4abdcf6d990a7adf43f
delveri <delveri.chick@anthem.com>
2018-02-14 15:48:35 -0500

    Work in progress: Draw first prototype of ship

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import {Game,splashScreen, gameScreen, gameCtx} from './utils/game';
 import {controls} from './utils/controls';
+
 document.body.addEventListener('keyup', (event)=>{
     if(event.keyCode == controls.startKey){
       splashScreen.className += 'fadesplash';
@@ -8,21 +9,17 @@ document.body.addEventListener('keyup', (event)=>{
         gameScreen.style.display = 'block';
       }, 2000);
 
-
       /* At this point we dont want the game to start playing before we see
           the game screen so we use a delay to give room for our transition
           to occur
       */
       //let playerName = prompt('Enter Player Name');  We can fetch player name
       setTimeout(function(){
-        gameCtx.fillStyle = "teal";
-        gameCtx.fillRect(0,0,innerWidth,innerHeight);
+        // gameCtx.fillStyle = "teal";
+        // gameCtx.fillRect(0,0,innerWidth,innerHeight);
         splashScreen.className = "";
         const game = new Game();
-        game.play();
-        // game.pause(function(){
-        //   console.log('Game Paused');
-        // });
+          game.play();
       }, 2000);
 
 
