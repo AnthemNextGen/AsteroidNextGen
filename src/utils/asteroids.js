acc62c770ac821a4587a37a4249d6a20b92354a0
Amos <AF66400@ad.wellpoint.com>
2018-02-20 12:49:27 -0500

    asteroid

--- src/utils/asteroids.js
+++ src/utils/asteroids.js
@@ -1,16 +1,21 @@
 import { gameCtx, gameScreen } from '../utils/game';
 export function drawAsteroid() {
-  gameCtx.fillStyle = 'black';
-  gameCtx.beginPath();
-  gameCtx.moveTo(30, 50);
-  gameCtx.lineTo(60, 30);
-  gameCtx.lineTo(70, 50);
-  gameCtx.lineTo(70, 60);
-  gameCtx.lineTo(60, 70);
-  gameCtx.lineTo(60, 80);
-  gameCtx.lineTo(40, 80);
-  gameCtx.lineTo(40, 70);
-  gameCtx.lineTo(30, 70);
-  gameCtx.closePath();
-  gameCtx.stroke();
+  //gameCtx.fillStyle = 'black';
+  for(var i = 0; i < 5; i++){
+    var x = Math.random()* innerWidth;
+    var y = Math.random()* innerHeight;
+    gameCtx.beginPath();
+    gameCtx.moveTo(x, y);
+    gameCtx.lineTo(60, 30);
+    gameCtx.lineTo(70, 50);
+    gameCtx.lineTo(70, 60);
+    gameCtx.lineTo(60, 70);
+    gameCtx.lineTo(60, 80);
+    gameCtx.lineTo(40, 80);
+    gameCtx.lineTo(40, 70);
+    gameCtx.lineTo(30, 70);
+    gameCtx.closePath();
+    gameCtx.strokeStyle = 'blue';
+    gameCtx.stroke();
+  }
 }
